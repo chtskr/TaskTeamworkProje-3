648f9171859921127e120ae582a7bd58256a1c8e
chtskr <122110471+chtskr@users.noreply.github.com>
2023-03-24 16:51:14 +0300

    Add files via upload

---
+++ CamelCase.java
@@ -0,0 +1,55 @@
+package PROJECTS.PROJECTS03;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CamelCase {
+
+    /*
+     TASK ->
+       First letter to upper case
+        parameter is one String
+        return type is String
+        words are delimited by a space.
+        All the words first letter should be camel case
+
+       input:
+
+           I lIVe in uSa
+
+      Result should be
+
+           I Live In Usa
+
+     */
+
+    /*
+            CamelCase : camelcase adında Kendisine gönderilen bir cümledeki kelimelerin sadece ilk karakterlerini
+            büyük harfe çevirip return eden  method create ediniz.
+            input: I lIVe in uSa
+            output: I Live In Usa
+
+     */
+    public static void main(String[] args) {
+
+
+        //System.out.println(camelCase());
+        Scanner scanner =   new Scanner(System.in);
+        System.out.println("bir cümle giriniz : ");
+        String cumle= scanner.nextLine();
+        System.out.println(cumle.substring(0).toUpperCase(Locale.ROOT));
+
+    }
+
+    private static void camelCase() {
+
+
+
+
+    }
+
+
+    }
+
+
+
---
+++ ThreeEvenOdd.java
@@ -0,0 +1,47 @@
+package PROJECTS.PROJECTS03;
+
+public class ThreeEvenOdd {
+
+    /*
+
+    Given an int array as a parameter
+    return true if the array contains either 3 even or 3 odd values .
+
+    for Example:
+
+    intArray([2, 1, 3, 5]) result should be true
+    intArray([2, 1, 2, 5]) result should be  false
+    intArray([2, 4, 2, 5]) result should be  true
+
+     */
+
+
+    /*
+   TASK ->
+   Parametre olarak int dizisi verildiğinde
+   array 3 tane çift veya 3 tane  tek değer içeriyorsa true return eden threeEvenOrOdd method create ediniz.
+
+
+   Örnek:
+
+   intArray([2, 1, 3, 5]) sonuc  true olmalı
+   intArray([2, 1, 2, 5]) sonuc   false olmalı
+   intArray([2, 4, 2, 5]) sonuc   true olmalı
+
+    */
+    public static void main(String[] args) {
+
+        threeEvenOrOdd();
+
+    }
+
+    private static int threeEvenOrOdd() {
+
+        int[] array = {2, 4, 2, 5};
+
+
+
+        return 0;
+    }
+
+}
